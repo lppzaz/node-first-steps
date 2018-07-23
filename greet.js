91c98e75046d49a530c555a674ce3d144f34b605
lppzaz <cobus.pienaar@gmail.com>
2018-07-23 14:39:06 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,8 @@
+var greet = function(name){
+var greetNeighbour = 'Hello'+','+' '+ name;
+return greetNeighbour
+}
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
---
+++ greet_xola.js
@@ -0,0 +1,11 @@
+
+var figlet = require('figlet');
+ 
+figlet('Cobus Rocks!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
